8be102eeda77445dcceea002c5956087901d2ae3
lookingforcharlie <charliefeng2020@gmail.com>
2023-07-18 15:17:24 -0400

    go production

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,4 @@
-// export const API_URL ='https://';
+export const API_URL =
+  'https://fullstackweatherappserver-production.up.railway.app/';
 
-export const API_URL = 'http://localhost:4000';
+// export const API_URL = 'http://localhost:4000';
